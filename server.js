38e5883a1b4f0a16faa55a1bea112c8cf65f3341
Nikita Demin <nikita@intspirit.com>
2017-01-25 18:09:19 +0300

    added es6 server

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-import path from 'path';
 import Express from 'express';
-import React from 'react';
-import { createStore } from 'redux';
+import http from 'http';
+
+const app = new Express();
+const server = new http.Server(app);
+
+server.listen(5000, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.info('==> 💻  Open http://%s:%s in a browser to view the app.', 'localhost', 5000);
+});

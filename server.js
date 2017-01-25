f37514e3be063c2a5128816dfdee1e5ba5ad7eda
Nikita Demin <nikita@intspirit.com>
2017-01-25 18:11:40 +0300

    added es6 server1

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import http from 'http';
 const app = new Express();
 const server = new http.Server(app);
 
+app.use('*', (req, res) => {
+  res.json({response: 'YOU ARE GOOD'});
+});
+
 server.listen(5000, (err) => {
   if (err) {
     console.error(err);
